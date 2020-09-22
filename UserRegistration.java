5184dd3e223d727e67cd52e79bf8bb6c8757ab90
hesho <hesho@hesho-pc.burawi.local>
2020-09-22 21:28:09 +0200

    Basic logins plus uni codes

---
+++ UserRegistration.java
@@ -0,0 +1,43 @@
+package ia;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class UserRegistration {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UserRegistration window = new UserRegistration();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public UserRegistration() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
